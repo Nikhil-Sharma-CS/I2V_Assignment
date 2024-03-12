f0003a64108166bac8d0956b9c1ff014ef4d35f8
Nikhil Sharma <nikhilsharma9076@gmail.com>
2024-03-12 10:18:02 +0530

    first commit

---
+++ TestAngular/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
+import { HashComponent } from './hash/hash.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    HashComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'hash', component: HashComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
+    ProductModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Test_DotNet/Controllers/HashController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+
+namespace Test_DotNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HashController : Controller
+    {
+        [HttpGet("{firstName}")]
+        public JsonResult GetHash(string firstName)
+        {
+            string input = firstName;
+            byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            
+            using (var hash = SHA256.Create())
+            {
+                byte[] hashedBytes = hash.ComputeHash(inputBytes);
+                
+                // Return a HashResponse Object which contains the hashString (alphanumeric, without '-') converted from hashedBytes
+                var response = new HashResponse
+                {
+                    Hash = BitConverter.ToString(hashedBytes).Replace("-", "")
+                };
+                return new JsonResult(response);
+            }
+        }
+
+    }
+
+    public class HashResponse
+    {
+        public string Hash { get; set; }
+    }
+}
